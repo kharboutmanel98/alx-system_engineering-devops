66bb97dbc3e51070cff6776526a26620ac626256
kharboutmanel98 <kharbout2017@gmail.com>
2024-06-23 23:35:33 +0200

    commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fixed the Apache 500 typo error in wordpress
-exec { 'fix typo':
-  onlyif  => 'test -e /var/www/html/wp-settings.php',
-    command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      }
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
+    }
